d0887444b4ccd2c4418e15d819cb3a6b2bb2a436
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-01-27 17:09:20 -0300

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,8 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
 import React from "react";
+import { useRouter } from 'next/router';
 import appConfig from "../config.json";
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+
 
 function Titulo(props) {
   console.log(props);
@@ -66,10 +38,10 @@ function Titulo(props) {
 export default function PaginaInicial() {
   //const username = 'pinalli';
   const [username, setUsername] = React.useState("pinalli");
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -103,7 +75,12 @@ export default function PaginaInicial() {
         >
           {/* Formulário */}
           <Box
-            as="form"
+            as='form'
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log('Alguém submeteu o form');
+              roteamento.push('/chat');
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -170,6 +147,7 @@ export default function PaginaInicial() {
 
           {/* Photo Area */}
           <Box
+
             styleSheet={{
               display: "flex",
               flexDirection: "column",
